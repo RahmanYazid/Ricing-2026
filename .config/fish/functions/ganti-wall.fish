cb2037638360a17b56d619a7433f51f46ef6a294
RahmanYazid <yaziddr72@gmail.com>
2026-06-01 03:17:11 +0700

    fix: remove old .config folder

--- .config/fish/functions/ganti-wall.fish
+++
@@ -1,9 +0,0 @@
-function ganti-wall
-    wal -i $argv[1]
-    # Update warna Waybar
-    pkill -USR2 waybar
-    # Restart Dunst
-    killall dunst; and dunst &
-    # Set wallpaper (swww)
-    awww img $argv[1] --transition-type wipe
-end
